fb06bad28910b5918f819f4092867a72fc632ab8
Rayane <secours@a-999904.ups.u-psud.fr>
2024-12-03 17:44:00 +0100

    création du descripteur fdsocket

--- udp_server.c
+++ udp_server.c
@@ -8,28 +8,51 @@
 
 /* Port local du serveur */
 #define PORT 9600
-   
+
+// truc pour locker adresse ip
+/*
+struct sockaddr_in {
+    short            sin_family;   // famille d'adresse : AF_INET
+    unsigned short   sin_port;     // numéro de port
+    struct in_addr   sin_addr;     // adresse IP
+    char             sin_zero[8];  //? inutilisé
+};
+struct in_addr {
+    unsigned long s_addr;  // initialiser avec inet_aton()
+};
+*/
+
 int main(int argc, char *argv[]) {
     /**
       * variables du serveur
       *
       * à déclarer : 
-        - sockfd descripteur de socket
-        - structure d'adresse locale du serveur
-        - structure d'adresse du client
-        - taille de l'adresse du client
+        - sockfd descripteur de socket OK
+        - structure d'adresse locale du serveur OK
+        - structure d'adresse du client OK
+        - taille de l'adresse du client OK
      **/
-     
-     /**
-      * code du serveur
-      *
-      * - Ouvrir le socket du serveur
-      * - Remplir la structure d'adresse locale du serveur :
-          - la famille d'adresse
-          - l'adresse IP
-          - le ort
-      * - Spécifier l'adresse locale du socket du serveur
-     **/
+
+    /** 0 = entête/id du protocole ip
+      * création du descripteur de socket
+      */
+    int fdsocket =  socket(PF_INET /* pourquoi pas AF_INET? */, SOCK_DGRAM /*Pour UDP, mode datagram*/, 0);
+    if (fdsocket == -1) {
+      perror("échec de création de la socket (Serveur udp)");
+    };
+
+
+   // faire le bind 
+    /**
+    * code du serveur
+    *
+    * - Ouvrir le socket du serveur
+    * - Remplir la structure d'adresse locale du serveur :
+        - la famille d'adresse
+        - l'adresse IP
+        - le ort
+    * - Spécifier l'adresse locale du socket du serveur
+    **/
 
      while (1) {
         break;
